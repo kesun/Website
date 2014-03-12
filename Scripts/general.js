c04367a9dc144ec8cdb7e1dcaac42fde49c4b5c1
Ke Sun <kesun15@gmail.com>
2014-03-11 22:04:58 -0700

    Basic structure complete.

--- Scripts/general.js
+++ Scripts/general.js
@@ -1,18 +1,30 @@
 $(document).ready(function() {
    $('.div1').mouseenter(function() {
        $(this).animate({
-           height: '+=370px'
+           height: '400px'
        });
 	   jQuery('.button', this).fadeIn('slow');
-   });
-   $('.div1').mouseleave(function() {
+	   jQuery('.buttonText', this).fadeIn('fast');
+	   $('#left-wing').rotate({animateTo:10, duration: 200});
+	   $('#right-wing').rotate({animateTo:-10, duration: 200});
+   }).mouseleave(function() {
        $(this).animate({
-           height: '-=370px'
+           height: '30px'
        }); 
 	   jQuery('.button', this).hide();
+	   jQuery('.buttonText', this).hide();
+	   $('#left-wing').rotate({animateTo:-10});
+	   $('#right-wing').rotate({animateTo:10});
    });
    $('#about-button').click(function(){
-        $('.aboutBox').slideToggle('fast');
+	   $('#about-banner').animate({
+           height: '30px'
+       }); 
+	   $('.button').hide();
+	   $('.buttonText').hide();
+	   $('#left-wing').rotate({animateTo:-10});
+	   $('#right-wing').rotate({animateTo:10});
+       $('.aboutBox').slideToggle('fast');
    });
    $('#close-about').click(function(){
 	   $('.aboutBox').slideToggle('fast');
